344bf22f5025eef27bb58f2ea2008b941dbebb09
Francisco.V98 <frncsico@gmail.com>
2024-04-17 17:47:13 -0400

    implement models, service and riverpod

---
+++ lib/infrastructure/service/list_movies_services.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:the_movie_db_app/infrastructure/models/list_movies_model.dart';
+
+class ListMoviesService {
+  var dio = Dio();
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/';
+
+  Future<ListMoviesModel> getMovieListPopular() async {
+    String url = '$baseUrl/popular';
+
+    try {
+      Response response = await dio.get(url);
+      if (response.statusCode == 200) {
+        return ListMoviesModel.fromJson(response.data);
+      } else {
+        throw Exception('Failed to load List Movies Popular: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load List Movies Popular: $e');
+    }
+  }
+}
